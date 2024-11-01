0163f7aedadbf71d8752b1fa24c5e2f7dc89eb5b
JesusGPriego <suleware@gmail.com>
2024-11-01 18:04:25 +0100

    project cleanup

--- src/main/java/com/suleware/springboot/app/springboot_crud/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.suleware.springboot.app.springboot_crud.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.suleware.springboot.app.springboot_crud.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-    
-}
